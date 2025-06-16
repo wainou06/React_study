899a46f36b3ec6c20cd2fb4439dfe98c2650a79c
wainou06 <wainou.6@gmail.com>
2025-06-16 14:45:27 +0900

    up

---
+++ React_study/ch12/src/ToolkitCounter/Counter.jsx
@@ -0,0 +1,10 @@
+function Counter() {
+   return (
+      <div>
+         <button>증가</button>
+         <button>감소</button>
+      </div>
+   )
+}
+
+export default Counter
---
+++ React_study/ch12/src/ToolkitCounter/counterSlice.js
@@ -0,0 +1,6 @@
+import { createSlice } from '@reduxjs/toolkit'
+const counterSlice = createSlice({
+    name:,
+    initialState:,
+    reducers:
+})
